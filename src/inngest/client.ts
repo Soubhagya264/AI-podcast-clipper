c19328fbd3e67368b1a08699f2ee2c67e9bcfb53
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2025-07-15 21:02:01 +0530

    fix: Inngest

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -3,5 +3,5 @@ import { Inngest } from "inngest";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ 
-    eventKey:process.env.NEXT_PUBLIC_INNGEST_EVENT_KEY,
+    eventKey:process.env.INNGEST_EVENT_KEY,
     id: "Ai-podcast-clipper" });
